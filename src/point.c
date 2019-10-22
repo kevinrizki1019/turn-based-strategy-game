05f303e7788a05b4b61eef3fc2e5df15dce9f431
Okugata Fahmi <otasrit@gmail.com>
2019-10-22 12:31:52 +0700

    Add file ADT Point

---
+++ src/point.c
@@ -0,0 +1,32 @@
+/* File: point.h */
+/* *** Implementasi ABSTRACT DATA TYPE POINT *** */
+#include "point.h"
+#include <stdio.h>
+
+
+/* *** DEFINISI PROTOTIPE PRIMITIF *** */
+/* *** Konstruktor membentuk POINT *** */
+POINT MakePOINT (int X, int Y)
+/* Membentuk sebuah POINT dari komponen-komponennya */
+{
+    /* Kamus Lokal */
+    POINT res;
+
+    /* Algoritma */
+    Absis(res)=X;
+    Ordinat(res)=Y;
+    return res;
+}
+
+/* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */
+void TulisPOINT (POINT P)
+/* Nilai P ditulis ke layar dengan format "(X,Y)"
+   tanpa spasi, enter, atau karakter lain di depan, belakang,
+   atau di antaranya
+   Output X dan Y harus dituliskan dalam bilangan riil dengan 2 angka di belakang koma.
+*/
+/* I.S. P terdefinisi */
+/* F.S. P tertulis di layar dengan format "(X,Y)" */
+{
+    printf("(%d,%d)",Absis(P),Ordinat(P));
+}
---
+++ src/point.h
@@ -0,0 +1,31 @@
+/* File: point.h */
+/* *** Definisi ABSTRACT DATA TYPE POINT *** */
+
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct {
+	int X; /* absis   */
+	int Y; /* ordinat */
+} POINT;
+
+/* *** Notasi Akses: Selektor POINT *** */
+#define Absis(P) (P).X
+#define Ordinat(P) (P).Y
+
+/* *** DEFINISI PROTOTIPE PRIMITIF *** */
+/* *** Konstruktor membentuk POINT *** */
+POINT MakePOINT (int X, int Y);
+/* Membentuk sebuah POINT dari komponen-komponennya */
+
+/* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */
+void TulisPOINT (POINT P);
+/* Nilai P ditulis ke layar dengan format "(X,Y)"
+   tanpa spasi, enter, atau karakter lain di depan, belakang,
+   atau di antaranya
+   Output X dan Y harus dituliskan dalam bilangan riil dengan 2 angka di belakang koma.
+*/
+/* I.S. P terdefinisi */
+/* F.S. P tertulis di layar dengan format "(X,Y)" */
+
+#endif
