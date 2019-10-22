d249f6a426162bfd83f5c4292e1f84e94e4352d3
okugatafahmi <otasrit@gmail.com>
2019-10-22 13:26:53 +0700

    Edit

--- src/tabbangunan.h
+++ src/tabbangunan.h
@@ -1,10 +1,10 @@
-/* Nama file: bangunan.h */
+/* Nama file: tabbangunan.h */
 /* Copyright: Kelompok 11 K-1 IF2110 2019/2020 */
-
-#include "bangunan.h"
 
 #ifndef __TABBANGUNAN_H__
 #define __TABBANGUNAN_H__
+
+#include "bangunan.h"
 
 // Banyaknya bangunan, angka disesuain sama spek
 #define IdxMax 29 
@@ -12,12 +12,17 @@
 
 typedef struct {
     BANGUNAN TB[IdxMax+1];
-    int Neff;
 } TabBANGUNAN;
+/* Tabel of BANGUNAN yang dideklarasikan secara implisit statik
 
 /* ********** SELEKTOR ********** */
-#define Neff(T)   (T).Neff
 #define TI(T)     (T).TI
 #define Elmt(T,i) (T).TI[(i)]
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create tabel kosong  */
+void MakeEmpty (TabBANGUNAN * T);
+/* I.S. T sembarang */
+/* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1. Isi tabel adalah ValTabUndef */
 
 #endif
