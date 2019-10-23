573de873f6e6c8fc62c19ff312523da455a3f948
kevin1019 <kevinrizkimohammad@gmail.com>
2019-10-23 23:08:38 +0700

    Add realisasi primitif dari ADT bangunan

--- src/bangunan.c
+++ src/bangunan.c
@@ -2,3 +2,179 @@
 /* Copyright: Kelompok 11 K-1 IF2110 2019/2020 */
 
 #include "bangunan.h"
+#include <stdio.h>
+
+/* ********** SELEKTOR ********** */
+int GetNilaiPenambahanPasukan (BANGUNAN B)
+// Mengembalikan nilai A bergantung pada JenisBangunan(B) dan Level(B)
+{
+    if (JenisBangunan(B) == 'C') {
+        return ((Level(B) * 5) + 5);
+    } else if (JenisBangunan(B) == 'T') {
+        if (Level(B) == 1) {
+            return 5;
+        } else if (Level(B) == 2) {
+            return 10;
+        } else if (Level(B) == 3) {
+            return 20;
+        } else if (Level(B) == 4) {
+            return 30;
+        }        
+    } else if (JenisBangunan(B) == 'F') {
+        return (Level(B) * 10);
+    } else if (JenisBangunan(B) == 'V') {
+        return (Level(B) * 5);
+    }
+}
+int GetMaksimumPenambahanPasukan (BANGUNAN B)
+// Mengembalikan nilai M bergantung pada JenisBangunan(B) dan Level(B)
+{
+    if (JenisBangunan(B) == 'C') {
+        return ((Level(B) * 20) + 20);
+    } else if (JenisBangunan(B) == 'T') {
+        return ((Level(B) * 10) + 10);
+    } else if (JenisBangunan(B) == 'F') {
+        return (Level(B) * 20);
+    } else if (JenisBangunan(B) == 'V') {
+        return ((Level(B) * 10) + 10);
+    }
+}
+int GetPasukanAwal (BANGUNAN B) 
+// Prekondisi: JenisBangunan(B) harus sudah terdefinisi
+// Mengembalikan nilai U bergantung pada JenisBangunan(B) dan Level(B)
+{
+    if (JenisBangunan(B) == 'C') {
+        return 40;
+    } else if (JenisBangunan(B) == 'T') {
+        return 30;
+    } else if (JenisBangunan(B) == 'F') {
+        return 80;
+    } else if (JenisBangunan(B) == 'V') {
+        return 20;
+    }
+}
+
+/* ********** VALIDATOR ********** */
+boolean IsJenisBangunanValid (char CC)
+// JenisBangunan valid: CC == [C, T, F, V]
+{
+    return (CC == 'C' || CC == 'T' || CC == 'F' || CC == 'V');
+}
+boolean IsPosisiValidByMap (POINT posisi, int tinggi, int lebar)
+// Posisi valid: absis(posisi) <= lebar(peta) && ordinat(posisi) <= tinggi(peta)
+{
+    return ((Absis(posisi) <= lebar) && (Ordinat(posisi) <= tinggi));
+}
+boolean IsJumlahPasukanValid (int jml_pasukan)
+// JumlahPasukan valid: jml_pasukan <= MAX_PASUKAN
+{
+    return (jml_pasukan <= MAX_PASUKAN);
+}
+boolean IsLevelValid (int lvl)
+// Level valid: 1 <= lvl <= 4
+{
+    return ((1 <= lvl) && (lvl <= 4));
+}
+
+/* ********** KONSTRUKTOR ********** */
+BANGUNAN MakeBANGUNAN (char jenis_bangunan, POINT posisi)
+{
+    BANGUNAN B;
+    JenisBangunan(B) = jenis_bangunan;
+    Posisi(B) = MakePOINT (Absis(posisi), Ordinat(posisi));
+    JumlahPasukan(B) = GetPasukanAwal(B);
+    Level(B) = 1;
+    SetPertahanan(&B);
+    return B;
+}
+
+/* ********** SETTER ********** */
+void SetPertahanan (BANGUNAN *B)
+// Prekondisi: JenisBangunan(*B) harus suah terdefinisi
+// Mengubah nilai status pertahanan sesuai levelnya
+{
+    if (JenisBangunan(*B) == 'C') {
+        Pertahanan(*B) = false;
+    } else if (JenisBangunan(*B) == 'T') {
+        Pertahanan(*B) =  true;
+    } else if (JenisBangunan(*B) == 'F') {
+        Pertahanan(*B) = (Level(*B) >= 3);
+    } else if (JenisBangunan(*B) == 'V') {
+        Pertahanan(*B) = false;
+    }
+}
+
+/* ********** OPERASI LAINNYA ********** */
+void TambahSatuLevel (BANGUNAN *B) 
+{
+    if (IsLevelValid(Level(*B) + 1)) {
+        Level(*B)++;
+    }
+}
+
+void TambahJumlahPasukan (BANGUNAN *B, int jumlah_penambahan)
+{
+    if (JumlahPasukan(*B) < GetMaksimumPenambahanPasukan(*B)) {
+        JumlahPasukan(*B) += jumlah_penambahan;
+    }
+}
+
+void KurangJumlahPasukan (BANGUNAN *B, int jumlah_pengurangan)
+{
+    if (JumlahPasukan(*B) - jumlah_pengurangan >= 0) {
+        JumlahPasukan(*B) -= jumlah_pengurangan;
+    }
+}
+
+void TulisBangunan (BANGUNAN B)
+{
+    if (JenisBangunan(B) == 'C') {
+        printf("Castle ");
+    } else if (JenisBangunan(B) == 'T') {
+        printf("Tower ");
+    } else if (JenisBangunan(B) == 'F') {
+        printf("Fort ");
+    } else if (JenisBangunan(B) == 'V') {
+        printf("Village ");
+    }
+    TulisPOINT(Posisi(B));
+    printf(" %d lv. %d", JumlahPasukan(B), Level(B));
+}
+
+int main() {
+    // BANGUNAN B1;
+    // POINT P;
+
+    // Absis(P) = 13;
+    // Ordinat(P) = 2;
+    // B1 = MakeBANGUNAN('V', P);
+    // TulisBangunan(B1);
+    // printf("\n");
+
+    // printf("%d\n", Pertahanan(B1));
+    // Pertahanan(B1) = true;
+    // printf("%d\n", Pertahanan(B1));
+    // SetPertahanan(&B1);
+    // printf("%d\n", Pertahanan(B1));
+
+    // TulisBangunan(B1);
+    // printf("\n");
+    // TambahSatuLevel(&B1);
+    // TulisBangunan(B1);
+    // printf("\n");
+
+    // TambahJumlahPasukan(&B1, 10);
+    // TulisBangunan(B1);
+    // printf("\n");
+    // TambahJumlahPasukan(&B1, 10);
+    // TulisBangunan(B1);
+    // printf("\n");
+
+    // KurangJumlahPasukan(&B1, 20);
+    // TulisBangunan(B1);
+    // printf("\n");
+    // KurangJumlahPasukan(&B1, 20);
+    // TulisBangunan(B1);
+    // printf("\n");
+    return 0;
+}
--- src/bangunan.h
+++ src/bangunan.h
@@ -14,33 +14,34 @@ typedef struct {
     POINT Posisi;
     int JumlahPasukan;
     int Level;
+    boolean P; // Nilai atribut P yaitu Pertahanan
 } BANGUNAN;
 /* CATATAN:
 Setelah dipikir-pikir ada beberapa attribut yg bisa didrop disini
 1. ID, digantiin dengan indeks bangunan pada tab bangunan
 2. Pemilik, digantiin dengan list linier
-3. A, M, P, dan U, didapet dari kombinasi JenisBangunan dan level
+3. A, M, dan U, didapet dari kombinasi JenisBangunan dan level
+4. Khusus untuk P karena ada skill shield up mesti dibuat atribut status pertahanan yes/no
 */
 
 /* ********** SELEKTOR ********** */
-#define JenisBangunan(B) (B).JenisBangunan
-#define Posisi(B) (B).Posisi
-#define JumlahPasukan(B) (B).JumlahPasukan
-#define Level(B) (B).Level
+#define JenisBangunan(B)    (B).JenisBangunan
+#define Posisi(B)           (B).Posisi
+#define JumlahPasukan(B)    (B).JumlahPasukan
+#define Level(B)            (B).Level
+#define Pertahanan(B)       (B).P
 
 int GetNilaiPenambahanPasukan (BANGUNAN B);
 // Mengembalikan nilai A bergantung pada JenisBangunan(B) dan Level(B)
 int GetMaksimumPenambahanPasukan (BANGUNAN B);
 // Mengembalikan nilai M bergantung pada JenisBangunan(B) dan Level(B)
-boolean GetPertahanan (BANGUNAN B);
-// Mengembalikan nilai P bergantung pada JenisBangunan(B) dan Level(B)
 int GetPasukanAwal (BANGUNAN B);
 // Mengembalikan nilai U bergantung pada JenisBangunan(B) dan Level(B)
 
 /* ********** VALIDATOR ********** */
 boolean IsJenisBangunanValid (char CC);
 // JenisBangunan valid: CC == [C, T, F, V]
-boolean IsPosisiValid (POINT posisi);
+boolean IsPosisiValidByMap (POINT posisi, int tinggi, int lebar);
 // Posisi valid: absis(posisi) <= lebar(peta) && ordinat(posisi) <= tinggi(peta)
 boolean IsJumlahPasukanValid (int jml_pasukan);
 // JumlahPasukan valid: jml_pasukan <= MAX_PASUKAN
@@ -48,9 +49,32 @@ boolean IsLevelValid (int lvl);
 // Level valid: 1 <= lvl <= 4
 
 /* ********** KONSTRUKTOR ********** */
-BANGUNAN MakeBANGUNAN (char tipe_bangunan, POINT posisi, int jml_pasukan);
+BANGUNAN MakeBANGUNAN (char jenis_bangunan, POINT posisi);
 // Prekondisi: semua parameter adalah valid untuk tipe BANGUNAN
 // Proses Membuat sebuah BANGUNAN dengan atribut sesuai parameter
 // untuk atribut Level dimulai dengan nilai 1
+
+/* ********** SETTER ********** */
+void SetPertahanan (BANGUNAN *B);
+// Mengubah nilai status pertahanan sesuai levelnya
+
+/* ********** OPERASI LAINNYA ********** */
+void TambahSatuLevel (BANGUNAN *B);
+// Menambah level bangunan sebesar 1 
+// selama nilai setelah penambahan masih valid
+
+void TambahJumlahPasukan (BANGUNAN *B, int jumlah_penambahan);
+// Menambahkan nilai JumlahPasukan(*B) sebesar jumlah_penambahan
+// tidak dilakukan penambahan jika JumlahPasukan(*B) >= M
+
+void KurangJumlahPasukan (BANGUNAN *B, int jumlah_pengurangan);
+// Menambahkan nilai JumlahPasukan(*B) sebesar jumlah_pengurangan
+// tidak dilakukan pengurangan jika JumlahPasukan(*B) - jumlah_pengurangan < 0
+
+
+void TulisBangunan (BANGUNAN B);
+// Menulis keterangan sebuah bangunan dengan format:
+// <JenisBangunan(B)> <Posisi(B)> <JumlahPasukan(B)> <Level(B)>
+// Tanpa spasi atau enter di awal dan di akhir
 
 #endif
