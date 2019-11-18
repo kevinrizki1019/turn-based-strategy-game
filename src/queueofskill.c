27e60b81b7bc9aa67cab58db3e5565582e74ff37
kevinrizki1019 <kevinrizkimohammad@gmail.com>
2019-11-18 22:19:30 +0700

    Fix conflict name on queueofskill

--- src/queueofskill.c
+++ src/queueofskill.c
@@ -1,51 +1,52 @@
 #include "queueofskill.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-boolean IsEmpty (Queue Q){
-    return(Head(Q)==Nil && Tail(Q)==Nil);
+boolean IsQueueEmpty (Queue Q){
+    return(Head(Q)==0 && Tail(Q)==0);
 }
 /* Mengirim true jika Q kosong: lihat definisi di atas */
-boolean IsFull (Queue Q){
+boolean IsQueueFull (Queue Q){
     int m = Head(Q) - Tail(Q);
-    return(m==1) || (m == (-1*MaxEl(Q)+1)); 
+    return(m==1) || (m == (-1*MaxElQueue(Q)+1)); 
 }
 /* Mengirim true jika tabel penampung elemen Q sudah penuh */
-/* yaitu mengandung elemen sebanyak MaxEl */
+/* yaitu mengandung elemen sebanyak MaxElQueue */
 
 /* *** Kreator *** */
 void CreateEmptyQueue (Queue * Q, int Max){
     (*Q).T = (infotype *) malloc ((Max+1)*sizeof(infotype));
     if ((*Q).T != NULL) {
-	    MaxEl(*Q) = Max;
-	    Head(*Q) = Nil;
-	    Tail(*Q) = Nil;
+	    MaxElQueue(*Q) = Max;
+	    Head(*Q) = 0;
+	    Tail(*Q) = 0;
 	} else{
-	    MaxEl(*Q) = Nil;
+	    MaxElQueue(*Q) = 0;
 	}
 }
 /* I.S. sembarang */
 /* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
 /* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
-/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* atau : jika alokasi gagal, Q kosong dg MaxElQueue=0 */
 /* Proses : Melakukan alokasi, membuat sebuah Q kosong */
 
 /* *** Destruktor *** */
 void DeAlokasi(Queue * Q){
-    MaxEl(*Q) = Nil;
+    MaxElQueue(*Q) = 0;
 	free((*Q).T);
 }
 /* Proses: Mengembalikan memori Q */
 /* I.S. Q pernah dialokasi */
-/* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
+/* F.S. Q menjadi tidak terdefinisi lagi, MaxElQueue(Q) diset 0 */
 
 /* *** Primitif Add/Delete *** */
 void Add (Queue * Q, infotype X){
-    if (IsEmpty(*Q)){	
+    if (IsQueueEmpty(*Q)){	
 		Tail(*Q) = 1;
 		Head(*Q) = 1;
 	} else{		
 		Tail(*Q) += 1;
-		if (Tail(*Q) == (*Q).MaxEl + 1) Tail(*Q) = 1;
+		if (Tail(*Q) == (*Q).MaxElQueue + 1) Tail(*Q) = 1;
 	}
 	InfoTail(*Q) = X;
 }
@@ -55,11 +56,11 @@ void Add (Queue * Q, infotype X){
 void Del (Queue * Q, infotype * X){
     (*X) = InfoHead(*Q);
 	if (Head(*Q) == Tail(*Q)){
-		Head(*Q) = Nil;
-		Tail(*Q) = Nil;
+		Head(*Q) = 0;
+		Tail(*Q) = 0;
 	} else {	
 		Head(*Q)++;
-		if (Head(*Q) == MaxEl(*Q) + 1) Head(*Q) = 1;
+		if (Head(*Q) == MaxElQueue(*Q) + 1) Head(*Q) = 1;
 	}
 }
 /* Proses: Menghapus X pada Q dengan aturan FIFO */
--- src/queueofskill.h
+++ src/queueofskill.h
@@ -5,7 +5,6 @@
 
 #include "boolean.h"
 
-#define Nil 0
 /* Konstanta untuk mendefinisikan address tak terdefinisi */
 
 /* Definisi elemen dan address */
@@ -16,9 +15,9 @@ typedef int addrQueue;   /* indeks tabel */
 typedef struct { infotype * T;   /* tabel penyimpan elemen */
                  addrQueue HEAD;  /* alamat penghapusan */
                  addrQueue TAIL;  /* alamat penambahan */
-                 int MaxEl;     /* Max elemen queue */
+                 int MaxElQueue;     /* Max elemen queue */
                } Queue;
-/* Definisi Queue kosong: HEAD=Nil; TAIL=Nil. */
+/* Definisi Queue kosong: HEAD=0; TAIL=0. */
 /* Catatan implementasi: T[0] tidak pernah dipakai */
 
 /* ********* AKSES (Selektor) ********* */
@@ -27,28 +26,28 @@ typedef struct { infotype * T;   /* tabel penyimpan elemen */
 #define Tail(Q) (Q).TAIL
 #define InfoHead(Q) (Q).T[(Q).HEAD]
 #define InfoTail(Q) (Q).T[(Q).TAIL]
-#define MaxEl(Q) (Q).MaxEl
+#define MaxElQueue(Q) (Q).MaxElQueue
 
 /* ********* Prototype ********* */
-boolean IsEmpty (Queue Q);
+boolean IsQueueEmpty (Queue Q);
 /* Mengirim true jika Q kosong: lihat definisi di atas */
-boolean IsFull (Queue Q);
+boolean IsQueueFull (Queue Q);
 /* Mengirim true jika tabel penampung elemen Q sudah penuh */
-/* yaitu mengandung elemen sebanyak MaxEl */
+/* yaitu mengandung elemen sebanyak MaxElQueue */
 
 /* *** Kreator *** */
 void CreateEmptyQueue (Queue * Q, int Max);
 /* I.S. sembarang */
 /* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
 /* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
-/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* atau : jika alokasi gagal, Q kosong dg MaxElQueue=0 */
 /* Proses : Melakukan alokasi, membuat sebuah Q kosong */
 
 /* *** Destruktor *** */
 void DeAlokasi(Queue * Q);
 /* Proses: Mengembalikan memori Q */
 /* I.S. Q pernah dialokasi */
-/* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
+/* F.S. Q menjadi tidak terdefinisi lagi, MaxElQueue(Q) diset 0 */
 
 /* *** Primitif Add/Delete *** */
 void Add (Queue * Q, infotype X);
@@ -58,7 +57,7 @@ void Add (Queue * Q, infotype X);
 void Del (Queue * Q, infotype * X);
 /* Proses: Menghapus X pada Q dengan aturan FIFO */
 /* I.S. Q tidak mungkin kosong */
-/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer; 
+/* F.S. X = 0ai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer; 
         Q mungkin kosong */
 
 #endif
